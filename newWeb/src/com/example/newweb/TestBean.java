ae13b74b8e48ce6a7f397765f919bfb36958dccb
zhonglei <zhonglei@local.com>
2022-01-05 15:28:33 +0800

    ver1.0

---
+++ newWeb/src/com/example/newweb/TestBean.java
@@ -0,0 +1,5 @@
+package com.example.newweb;
+
+public class TestBean {
+
+}
--- newWeb/src/com/example/newweb/servlet/MyServlet.java
+++ newWeb/src/com/example/newweb/servlet/MyServlet.java
@@ -12,10 +12,13 @@ public class MyServlet extends javax.servlet.http.HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        System.out.println("MyServlet execute doGet");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+        System.out.println("MyServlet execute doPost");
+
     }
 }
